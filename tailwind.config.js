cd4d709d1ccfbf9086ecccaf006dad186bd29218
ayomideRussianCode <ayindeayomide7@gmail.com>
2024-11-27 15:40:44 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        font1: ["Poppins", "sans-serif"],
+        font2: ["Akaya Telivigala", "system-ui"],
+        font3: ["Lexend", "sans-serif"],
+      },
+      colors: {
+        customWhite: "#fff",
+        customBlue: "#007AFF",
+        customBlack: "#1D1C1C"
+      },
+    },
+  },
+  plugins: [],
+};
