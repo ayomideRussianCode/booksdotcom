daf6b3b029a18a4fc501a5d846206f66b187f98d
ayomideRussianCode <ayindeayomide7@gmail.com>
2024-12-17 17:30:54 +0100

    testing for erro

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import reviews from "../Fixtures/Reviews.json";
 
-const Reviews = () => {
+function Reviews() {
+  const [curIndex, setCurIndex] = useState(0);
+  const reviewsPerView = 1;
+
+  const displayedReviews = reviews.slice(curIndex, curIndex + reviewsPerView);
+
+  const handleNext = () => {
+    setCurIndex((prevIndex) =>
+      prevIndex + reviewsPerView >= reviews.length
+        ? 0
+        : prevIndex + reviewsPerView
+    );
+  };
+
+  const handlePrev = () => {
+    setCurIndex((prevIndex) =>
+      prevIndex - reviewsPerView < 0
+        ? reviews.length
+        : prevIndex - reviewsPerView
+    );
+  };
+
   return (
-    <div>Reviews</div>
-  )
+    <section id="reviews">
+      <div className="container w-full h-64 justify-center border bg-customColor1 flex flex-col  my-8 rounded-none shadow-md overflow-hidden md:flex-row ">
+        <h2 className="text-xl font-font2 text-center my-6 uppercase text-customBlack ">
+          Customers Reviews
+        </h2>
+        <div className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 m-16  w- h-64 border bg-customWhite shadow-lg rounded-xl"></div>
+      </div>
+      
+    </section>
+  );
 }
-
-export default Reviews
+export default Reviews;
--- src/components/Services3.jsx
+++ src/components/Services3.jsx
@@ -1,7 +1,7 @@
 function Services3() {
     return (
       <section id="hero">
-        <div className="container border border-customColor2 bg-customBlue rounded-3xl  shadow-md overflow-hidden flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row ">
+        <div className="container border bg-customBlue rounded-3xl  shadow-md overflow-hidden flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row ">
           <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
             <h1 className=" mt-32 max-w-lg text-2xl font-medium text-customWhite font-font1 text-center md:text-5xl md:text-left ">
               Empowering Independent Authors
